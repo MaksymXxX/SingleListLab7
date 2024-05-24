dff38b94e44ca11aa9e16835441dc0e81ec7e946
melni <melni@MAKS>
2024-05-24 13:30:19 +0300

    Add project files.

---
+++ SingleList/Program.cs
@@ -0,0 +1,74 @@
+﻿using SingleList;
+using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        SingleLinkedList list = new SingleLinkedList();
+
+        Console.WriteLine("Введіть значення елементів списку (для завершення введіть порожній рядок):");
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input))
+                break;
+
+            if (short.TryParse(input, out short value))
+            {
+                list.AddAfterFirst(value);
+            }
+            else
+            {
+                Console.WriteLine("Будь ласка, введіть числове значення.");
+            }
+        }
+
+        Console.Write("Поточний список: ");
+        list.PrintList();
+
+        Console.Write("Введіть значення для знаходження першого входження елементу більше цього значення: ");
+        if (short.TryParse(Console.ReadLine(), out short threshold))
+        {
+            Node? foundNode = list.FindFirstGreaterThan(threshold);
+            Console.Write("Перше входження елементу більше {0}: ", threshold);
+            if (foundNode != null)
+            {
+                Console.WriteLine(foundNode.Data);
+            }
+            else
+            {
+                Console.WriteLine("Не знайдено елементу списку, що більше {0}.", threshold);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Будь ласка, введіть коректне числове значення.");
+        }
+
+        Console.Write("Сума елементів більших за середнє значення: ");
+        Console.WriteLine(list.SumGreaterThanAverage());
+
+        Console.Write("Новий список зі значень елементів менших за середнє значення: ");
+        SingleLinkedList newList = list.GetListLessThanAverage();
+        newList.PrintList();
+
+        list.RemoveElementsAfterMax();
+        Console.Write("Список після видалення елементів, що розташовані після максимального: ");
+        list.PrintList();
+
+        Console.WriteLine("Використання індексатора для доступу до елементів списку:");
+        try
+        {
+            Console.WriteLine("Елемент за індексом 0: " + list[0]);
+            Console.WriteLine("Елемент за індексом 1: " + list[1]);
+        }
+        catch (IndexOutOfRangeException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
---
+++ SingleList/SingleLinkedList.cs
@@ -0,0 +1,200 @@
+﻿namespace SingleList
+{
+    public class Node
+    {
+        public short Data { get; set; }
+
+        public Node? Next { get; set; }
+
+        public Node(short data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+
+    public class SingleLinkedList
+    {
+        private Node? head;
+
+        public SingleLinkedList()
+        {
+            head = null;
+        }
+
+        public void AddAfterFirst(short data)
+        {
+            Node newNode = new Node(data);
+
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                newNode.Next = head.Next;
+                head.Next = newNode;
+            }
+        }
+
+        public Node? FindFirstGreaterThan(short value)
+        {
+            Node? current = head;
+
+            while (current != null)
+            {
+                if (current.Data > value)
+                {
+                    return current;
+                }
+
+                current = current.Next;
+            }
+
+            return null;
+        }
+
+        public short SumGreaterThanAverage()
+        {
+            if (head == null)
+            {
+                return 0;
+            }
+
+            var average = GetAverageValue();
+
+            short sum = 0;
+            Node? current = head;
+
+            while (current != null)
+            {
+                if (current.Data > average)
+                {
+                    sum += current.Data;
+                }
+
+                current = current.Next;
+            }
+
+            return sum;
+        }
+
+        public SingleLinkedList GetListLessThanAverage()
+        {
+            SingleLinkedList newList = new SingleLinkedList();
+
+            if (head == null)
+            {
+                return newList;
+            }
+
+            double average = GetAverageValue();
+
+            Node? current = head;
+            while (current != null)
+            {
+                if (current.Data < average)
+                {
+                    newList.AddAfterFirst(current.Data);
+                }
+                current = current.Next;
+            }
+
+            return newList;
+        }
+
+        private double GetAverageValue()
+        {
+            int count = 0;
+            int totalSum = 0;
+            Node? current = head;
+
+            while (current != null)
+            {
+                totalSum += current.Data;
+                count++;
+                current = current.Next;
+            }
+
+            return (double)totalSum / count;
+        }
+
+        public void RemoveElementsAfterMax()
+        {
+            if (head == null)
+            {
+                return;
+            }
+
+            Node? current = head;
+            Node? maxNode = head;
+
+            while (current != null)
+            {
+                if (current.Data > maxNode.Data)
+                {
+                    maxNode = current;
+                }
+                current = current.Next;
+            }
+
+            if (maxNode != null)
+            {
+                maxNode.Next = null;
+            }
+        }
+
+        public short this[int index]
+        {
+            get
+            {
+                Node? current = head;
+                int count = 0;
+
+                while (current != null)
+                {
+                    if (count == index)
+                    {
+                        return current.Data;
+                    }
+                    count++;
+                    current = current.Next;
+                }
+
+                throw new IndexOutOfRangeException("Index was out of range.");
+            }
+            set
+            {
+                Node? current = head;
+                int count = 0;
+
+                while (current != null)
+                {
+                    if (count == index)
+                    {
+                        current.Data = value;
+                        return;
+                    }
+                    count++;
+                    current = current.Next;
+                }
+
+                throw new IndexOutOfRangeException("Index was out of range.");
+            }
+        }
+
+        public void PrintList()
+        {
+            Node? current = head;
+
+            while (current != null)
+            {
+                Console.Write(current.Data + " ");
+
+                current = current.Next;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
